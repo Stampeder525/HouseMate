d727f5a3384cfb9840d7fd9e9fa02c3bee4cdb66
Alex Erf <alexerf@umich.edu>
2017-09-23 15:44:51 -0400

    Got firebase sorta working

--- my-project/App.js
+++ my-project/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
 import { StackNavigator } from 'react-navigation';
 
 import SignUpProfile from './SignUpProfile.js';
 import SignUpBanking from './SignUpBanking.js';
 import SignUpNotifs from './SignUpNotifs.js';
+import ConfigWheel from './ConfigWheel.js';
+
+
+
 
 class App extends React.Component {
+    static navigationOptions = {
+      title: 'Welcome',
+    };
+    constructor(props) {
+        super(props);
+        console.log("FJSLDKFJKLSDJF");
+    }
   render() {
-    return (
-      <SignUpProfile />
-    );
+      return(
+          <View style={styles.container}>
+            <Button
+            title="Next"
+            onPress={() => {
+                this.props.navigation.navigate('Profile')
+            }}>
+            </Button>
+          </View>
+      );
   }
+
 }
 
 const styles = StyleSheet.create({
@@ -23,10 +44,13 @@ const styles = StyleSheet.create({
   },
 });
 
+
 const AppNav = StackNavigator({
+    App: { screen: App },
   Profile: { screen: SignUpProfile },
   Banking: { screen: SignUpBanking },
   Notifications: { screen: SignUpNotifs },
+  ConfigWheel: { screen: ConfigWheel }
 });
 
 export default AppNav;
--- my-project/SignUpProfile.js
+++ my-project/SignUpProfile.js
@@ -5,8 +5,19 @@ import { Button } from 'react-native';
 
 import SignUpBanking from './SignUpBanking.js'
 
+var api = require('./AccountCreation.js')
+
+
 export class SignUpProfile extends React.Component {
- 
+  constructor(props) {
+      super(props);
+      this.state = {
+          name: '',
+          email: '',
+          password: '',
+          confirmPassword: ''
+      };
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -14,19 +25,20 @@ export class SignUpProfile extends React.Component {
         <TextInput
           style={styles.text_field}
           placeholder="Name"
-          onChangeText={(text) => this.setState({text})}
+          onChangeText={(text) => {this.setState({name: text})}}
           underlineColorAndroid = 'rgba(0,0,0,0)'
         />
         <TextInput
           style={styles.text_field}
           placeholder="Email"
-          onChangeText={(text) => this.setState({text})}
+          value={this.state.email}
+          onChangeText={(text) => {this.setState({email: text})}}
           underlineColorAndroid = 'rgba(0,0,0,0)'
         />
         <TextInput
           style={styles.text_field}
           placeholder="Password"
-          onChangeText={(text) => this.setState({text})}
+          onChangeText={(text) => this.setState({password: text})}
           underlineColorAndroid = 'rgba(0,0,0,0)'
           secureTextEntry={true}
         />
@@ -34,21 +46,21 @@ export class SignUpProfile extends React.Component {
           style={styles.text_field}
 
           placeholder="Confirm Password"
-          onChangeText={(text) => this.setState({text})}
+          onChangeText={(text) => this.setState({confirmPassword: text})}
           underlineColorAndroid = 'rgba(0,0,0,0)'
           secureTextEntry={true}
         />
         <Button
             title="Next"
-            onPress={() =>
-            this.props.navigation.navigate('Banking')
-            }
+            onPress= {() => {
+                this.props.navigation.navigate('Banking')
+                api.createAccount(this.state)
+            }}
         />
       </View>
     );
   }
 }
-
 
 
 
